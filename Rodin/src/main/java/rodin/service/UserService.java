b639eb8f456485c947c27c446d9552737f4448e4
MyeongchanJin <jmcchan@naver.com>
2020-02-06 19:37:05 +0900

    MVC 모델 2에 따라 회원가입 로직 작성

--- Rodin/src/main/java/rodin/service/UserService.java
+++ Rodin/src/main/java/rodin/service/UserService.java
@@ -1,5 +1,9 @@
 package rodin.service;
 
-public interface UserService {
+import rodin.repository.vo.UserVo;
 
+public interface UserService {
+	boolean setUser(UserVo uvo);
+	UserVo getUserByEmailAndPasswordAtService(String email, String password);
+    // UserVo getUserByIdAtService(String id);
 }
